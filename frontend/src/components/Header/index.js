5f70b06898ab0c838519e7ef87a86b0c16ece107
kingofsweets <kingofsweetsx2k@gmail.com>
2021-08-27 16:47:56 +0300

    - header style

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,26 +1,50 @@
 import React, { useContext } from 'react'
-import {PageWrapper, HeadText} from './style'
-import './inde.css'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
-import { Button, withStyles } from '@material-ui/core'
+
+
+import sfedu_logo from '../../common/media/header/sfedu-logo.svg'
+
+import directions from '../../common/media/header/directions.svg'
+import home from '../../common/media/header/home.svg'
+import test from '../../common/media/header/test.svg'
+import profile from '../../common/media/header/profile.svg'
 
 
 export const Header = () =>{
     const {login,logout, rolek} = useContext(AuthContext)
         return(
-            <header>
-                <div className = "main_but_cont">
-                    <div className = "no_main_but_cont">
-                        <Link to = "/home"><Button variant = "contained" color = "primary">Главная</Button></Link>
-                        <Link to = "/profile"><Button variant = "contained" color = "primary">Личный кабинет</Button></Link>
-                        <Link to = "/test"><Button variant = "contained" color = "primary">Тестирование</Button></Link>
-                        <Link><Button variant = "contained" color = "primary">Направления</Button></Link>
+            <header class="header container">
+                <div className="outline outline_light"></div>
+                <div className="inline inline_light"></div>
+                <div className="header__logo">
+                    <img src={sfedu_logo} alt="" />
+                </div>
+                <div className = "header__menu">
+
+                    <div className = "header__menu header__menu_user">
+                        <Link className="header__button">
+                            <img src={directions} alt="" />
+                            <button>Направления</button>
+                        </Link>
+                        <Link to = "/test" className="header__button">
+                            <img src={test} alt="" />
+                            <button>Тестирование</button>
+                        </Link>
+                        <Link to = "/profile" className="header__button">
+                            <img src={profile} alt="" />
+                            <button>Личный кабинет</button>
+                        </Link>
+                        <Link to = "/home" className="header__button">
+                            <img src={home} alt="" />
+                            <button>Главная</button>
+                        </Link>
+
                     </div>
-                    <div>
+                    {/* <div>
                     {rolek === 1 ? <Link to="/admin/users"><Button variant = "contained" color = "default">Админ</Button></Link>: null}
                     <Button className = "leave" variant = "contained" color = "secondary" onClick = {logout}>Ливнуть</Button>
-                    </div>
+                    </div> */}
 
                 </div>
             </header>
