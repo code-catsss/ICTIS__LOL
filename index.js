e4fcd9054d633703e3469f922bad798e6ffcb78a
kingofsweets <kingofsweetsx2k@gmail.com>
2021-09-01 12:29:01 +0300

    - a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
+// const path = require('path');
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -12,12 +12,12 @@ app.use('/api/news', require('./routes/news.route'))
 app.use('/api/test', require('./routes/test.route'))
 app.use('/api/directions', require('./routes/directions.route'))
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static("build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//     });
+// }
 
 async function start(){
     try {
