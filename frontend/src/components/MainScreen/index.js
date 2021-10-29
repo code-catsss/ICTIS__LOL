4c7c869b62e0d3267bafd4965c9c70f816d3af37
kingofsweets <kingofsweetsx2k@gmail.com>
2021-10-29 10:39:29 +0300

    - fix lol

--- frontend/src/components/MainScreen/index.js
+++ frontend/src/components/MainScreen/index.js
@@ -140,9 +140,9 @@ export const  MainScreen = () =>{
                     https://github.com/code-catsss/ICTIS__LOL
                 </p>
                 <p className="footer__anchor">
-                    Во дворе трава
+                    Официальный сайт ЮФУ:
                     <br/>
-                    На траве дрова
+                    https://sfedu.ru
                 </p>
                 <h1 className="footer__anchor">
                     8(908)-179-35-83
